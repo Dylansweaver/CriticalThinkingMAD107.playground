8cfb007f8bb56303d8f4dcb96a9e9067d6f209d0
Dylansweaver <dweaver@e112-m-15.mchenry.edu>
2016-09-06 17:08:20 -0500

    CriticalThinkingMAD107

---
+++ Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var b = "add insult here"
+
+let a:String = "You are a "
+
+
+
+
+b = "donkey"
+
+print(a+b)
+
+b = "turtle"
+
+print(a+b)
+
+//continue to modify b to get different insults
+var w:Float = 0
+var x:Float = 4
+let y:Float = 7.2
+let z:Float = 4
+
+w = x + y + z
+
+print(w)
